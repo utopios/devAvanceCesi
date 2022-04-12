616a0831cfec15457ca566e0faf416e2f2802d64
ihab <ihab@utopios.net>
2022-04-12 14:22:34 +0200

    structure du service

--- api/src/main/java/cesi/api/formationapi/services/ToDoService.java
+++ api/src/main/java/cesi/api/formationapi/services/ToDoService.java
@@ -1,4 +1,19 @@
 package cesi.api.formationapi.services;
 
 public class ToDoService {
+    //Créer une todolist
+
+    //Créer une todoItem
+
+    //Récupérer la liste des todoLis
+
+    //Récupérer une todoList
+
+    //Récupérer les todoitems d'une todolist
+
+    //Récupérer une todoitem
+
+    //Supprimer une todolist, todoitems
+
+    //Supprimer une todoitem
 }
